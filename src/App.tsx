99297f7772bcc1b679007487f8872bb46d9810d7
diego.abreu <diego.abreu@artit.com.br>
2026-03-18 16:45:36 -0300

    feat: projeto inicial

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Meu sistema de pousadas</h1>
+//     </div>
+//   );
+// }
+export default function App() {
+  return (
+    <div style={{ padding: 24, background: "#fff", minHeight: "100vh" }}>
+      <h1 style={{ color: "#111" }}>Meu sistema de pousadas</h1>
+    </div>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>Buscar Hospedagem</h1>
+    </div>
+  );
+}
